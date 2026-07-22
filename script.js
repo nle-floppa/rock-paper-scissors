bd31c0512d0e30a27f8c60c3f77d7a406ad682f2
nle-floppa <brunnerwallace@gmail.com>
2026-07-21 20:12:05 -0400

    Add playRound Function and Fix Bugs

--- script.js
+++ script.js
@@ -1,23 +1,70 @@
 `use strict`
 
+let computerChoice;
 function getComputerChoice() {
     let outcome;
     let numOutcome = Math.floor(Math.random() * 3);
     if (numOutcome === 0) {
-        outcome = "Rock";
+        computerChoice = "rock";
     } else if (numOutcome === 1) {
-        outcome = "Paper";
+        computerChoice = "paper";
     } else {
-        outcome = "Scissors";
+        computerChoice = "scissors";
         }
-    return outcome;
+    return computerChoice;
     }
 
-console.log(getComputerChoice())
 
+let humanChoice
 function getHumanChoice() {
-    const choice = prompt("Rock, Paper, or Scissors?");
-    return choice;
+    humanChoice = prompt("Rock, Paper, or Scissors?");
+    console.log(humanChoice);
+    return humanChoice;
 }
 
-console.log(getHumanChoice());
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    // make sure to lowercase conditions in if statement before checking them
+    if (humanChoice.toLowerCase() === "rock") {
+        if (computerChoice === "paper") {
+            console.log("You lose! Paper beats rock");
+            computerScore++;
+        } else if (computerChoice === "scissors") {
+            console.log("You win! Rock beats scissors");
+            humanScore++;
+        } else {
+            console.log("Tie! No points awarded");
+        }
+
+    } else if (humanChoice.toLowerCase() === "paper") {
+        if (computerChoice === "scissors") {
+            console.log("You lose! Scissors beats paper");
+            computerScore++;
+        } else if (computerChoice === "rock") {
+            console.log("You win! Rock beats paper");
+            humanScore++;
+        } else {
+            console.log("Tie! No points awarded");
+        }
+
+    } else if (humanChoice.toLowerCase() === "scissors") {
+        if (computerChoice === "rock") {
+            console.log("You lose! Rock beats scissors");
+            computerScore++;
+        } else if (computerChoice === "paper") {
+            console.log("You win! Scissors beats paper");
+            humanScore++;
+        } else {
+            console.log("Tie! No points awarded");
+        }
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+console.log(humanScore);
+console.log(computerScore);
